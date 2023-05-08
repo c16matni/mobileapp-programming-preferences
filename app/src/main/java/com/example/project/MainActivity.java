b98542a231a031d327bc54ddec09f624c1d04373
SleepyBearIV <mattias.nicander.mn@gmail.com>
2023-05-08 15:13:38 +0200

    fixat så manka spara tex och läsa den i main

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+
+    public TextView textView;
 
     private Button button;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textView = (TextView) findViewById(R.id.Text);
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity2.preferenser, MODE_PRIVATE);
+        String textFromEditText = sharedPreferences.getString(MainActivity2.preferenser, "");
+        textView.setText(textFromEditText);
+
+
+
 
     }
 
--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -2,13 +2,39 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    public static final String preferenser = "Shared Preference";
+
+    private Button button;
+    private EditText eText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        eText = (EditText) findViewById(R.id.settingseditview);
+        button = (Button) findViewById(R.id.prefButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                SharedPreferences sharedP = getSharedPreferences(preferenser, MODE_PRIVATE);
+                SharedPreferences.Editor sharedPEdit = sharedP.edit();
+                sharedPEdit.putString(preferenser, eText.getText().toString()).apply();
+
+
+            }
+
+        });
     }
+
+
 }
